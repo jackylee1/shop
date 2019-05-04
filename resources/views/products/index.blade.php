749cc137d1f758ad720b53b0cfbd7937f21e8928
Maxim <maksa988ua@gmail.com>
2019-05-04 23:50:45 +0300

    Refactor product view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,45 +7,9 @@
                 @include('components._breadcrumb')
             </div>
             <div class="col-md-8 col-md-offset-2">
-                <div class="card">
-                    <div class="card-header">Products</div>
-                </div>
-
-                <div class="row mt-4">
+                <div class="row">
                     @foreach($products as $product)
-                    <div class="col-md-4">
-                        <div class="card mb-4 shadow-sm product-item">
-                            <div class="product-controls">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-danger"><i class="fas fa-heart"></i></button>
-                                    <button type="button" class="btn btn-sm btn-light"><i class="fas fa-balance-scale"></i></button>
-                                </div>
-                            </div>
-                            @if($product->cover)
-                            <img src="{{ asset($product->cover) }}" alt="{{ $product->title }}" class="bg-placeholder-img card-img-top">
-                            @endif
-                            <div class="card-body">
-                                <a href="{{ route('products.show', $product) }}" class="card-link">
-                                    {{ $product->title }}
-                                </a>
-                                <small class="text-muted">
-                                    {{ $product->category->title }}
-                                </small>
-                            </div>
-                            <div class="card-footer">
-                                <div class="d-flex justify-content-between align-items-center">
-
-                                    <span class="price">
-                                        <b>{{ price($product->price) }}</b> $
-                                    </span>
-
-                                    <button type="button" class="btn btn-sm btn-success">
-                                        <i class="fas fa-shopping-cart"></i> <span class="buy-text">Купить</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        @include('products._item')
                     @endforeach
                 </div>
             </div>
