74bc20e5bbc36ed63be33ea6ad0b1b5096969806
Maxim <maksa988ua@gmail.com>
2019-05-05 18:44:22 +0300

    Image Unit test

---
+++ tests/Unit/ImageTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Image;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ImageTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @var Image
+     */
+    protected $image;
+
+    /**
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->image = create(Image::class);
+    }
+
+    /** @test */
+    public function a_image_belongs_to_a_product()
+    {
+        $this->assertInstanceOf(Product::class, $this->image->product);
+    }
+
+    /** @test */
+    public function a_image_can_be_a_cover()
+    {
+        $this->assertTrue($this->image->is_cover);
+    }
+}
