2edc687aacbcf8f8ea3befc981bee5376b3f0662
Maxim <maksa988ua@gmail.com>
2019-07-23 14:02:12 +0300

    Correct word (buttom -> button)

--- resources/js/app/app.js
+++ resources/js/app/app.js
@@ -25,7 +25,7 @@ window.Vue = require('vue');
 Vue.component('bookmark-button', require('./components/BookmarkButtonComponent.vue').default);
 
 Vue.component('cart-button', require('./components/Cart/CartButtonComponent.vue').default);
-Vue.component('product-cart-button', require('./components/Cart/ProductCartButtom.vue').default);
+Vue.component('product-cart-button', require('./components/Cart/ProductCartButton.vue').default);
 Vue.component('cart-remove-button', require('./components/Cart/CartItemRemoveButtonComponent.vue').default);
 Vue.component('cart-item-count', require('./components/Cart/CartItemCount.vue').default);
 Vue.component('navbar-cart', require('./components/Cart/NavbarCartComponent.vue').default);
